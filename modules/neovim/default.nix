9e9051645ae81e2cc59204bbd2dc0b77fd941c77
EsotericSquishyy <jesselcobb@gmail.com>
2024-06-05 12:31:13 -0700

    cmp working for nvim now

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -105,25 +105,26 @@ in {
                 #luasnip.enable = true;
 
                 # Autocomplete
-                # nvim-cmp = {
-                #     enable = true;
-                #     mapping = {
-                #         "<C-Space>" = "cmp.mapping.complete()";
-                #         "<C-d>"     = "cmp.mapping.scroll_docs(-4)";
-                #         "<C-e>"     = "cmp.mapping.close()";
-                #         "<C-f>"     = "cmp.mapping.scroll_docs(4)";
-                #         "<CR>"      = "cmp.mapping.confirm({ select = true })";
-                #         "<S-Tab>"   = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-                #         "<Tab>"     = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-                #     };
-                #     autoEnableSources = true;
-                #     sources = [
-                #         { name = "nvim_lsp"; }
-                #         #{ name = "luasnip"; }
-                #         { name = "buffer"; }
-                #         { name = "path"; }
-                #     ];
-                # };
+                cmp = {
+                    enable = true;
+                    settings.mapping = {
+                        "<C-Space>" = "cmp.mapping.complete()";
+                        "<C-d>"     = "cmp.mapping.scroll_docs(-4)";
+                        "<C-e>"     = "cmp.mapping.close()";
+                        "<C-f>"     = "cmp.mapping.scroll_docs(4)";
+                        "<CR>"      = "cmp.mapping.confirm({ select = true })";
+                        "<S-Tab>"   = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+                        "<Tab>"     = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+                    };
+                    autoEnableSources = true;
+                    settings.sources = [
+                        { name = "nvim_lsp"; }
+                        #{ name = "luasnip"; }
+                        { name = "buffer"; }
+                        { name = "path"; }
+                    ];
+
+                };
 
                 # Buffer Bar
                 bufferline = {
