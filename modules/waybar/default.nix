ed307b85d01b102cc820763f34b45b666b083dab
EsotericSquishyy <jesselcobb@gmail.com>
2025-06-19 10:31:15 -0700

    unnixed waybar

--- modules/waybar/default.nix
+++ modules/waybar/default.nix
@@ -1,17 +1,22 @@
 # Waybar - Wayland bar
-{ pkgs, lib, config, ...} : {
-
+{ pkgs, pkgs-unstable, lib, config, ...}: {
     options.waybarModule = {
         enable = lib.mkEnableOption "enables waybarModule";
     };
 
     config = lib.mkIf config.waybarModule.enable {
-        programs.waybar = {
-            enable = true;
-            style = ./style.css;
-        };
+        home.packages = (with pkgs; [
+            # stable packages
+        ]) ++ (with pkgs-unstable; [
+            # unstable packages here
+        ]);
 
-        xdg.configFile."waybar/config".source = ./config;
+        home.file.".config/waybar".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/dotfiles/waybar";
+
+        # programs.waybar = {
+        #     enable = true;
+        #     style = ./style.css;
+        # };
+        # xdg.configFile."waybar/config".source = ./config;
     };
-
 }
